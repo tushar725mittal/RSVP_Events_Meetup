64159e06e95f7fc36f28d69fe177268a74e84d79
tushar725mittal <tushar725mittal@gmail.com>
2023-03-25 18:57:19 +0530

    linux chrome driver added

--- rsvp.py
+++ rsvp.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from utils import auth, events, attend, groups
+import sys
 
 
 # write main function
 def main():
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    driver = webdriver.Chrome("./chromedriver.exe", options=options)
+    driver = webdriver.Chrome("./chromedriver", options=options)
+
     auth.auth_login(driver)
     grp_links = groups.extract_groups(driver)
     eventLinks = events.get_event_links(driver, grp_links)
